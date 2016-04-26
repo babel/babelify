833dfc73452b63ec7b6ab9dca61a052889d8237b
Andres Suarez <zertosh@gmail.com>
2016-04-26 00:12:15 -0700

    Test assumptions about babel's source maps

---
+++ test/babel-source-maps.js
@@ -0,0 +1,47 @@
+var assert = require('assert');
+var babel = require('babel-core');
+var convert = require('convert-source-map');
+var fs = require('fs');
+var path = require('path');
+var pathIsAbsolute = require('path-is-absolute');
+var test = require('tap').test;
+
+// Validate assumptions about babel's source maps.
+
+var sourceFile = path.join(__dirname, 'bundle/index.js');
+assert(pathIsAbsolute(sourceFile));
+
+var sourceSrc = fs.readFileSync(sourceFile, 'utf8');
+
+test('babel source maps (only filename)', function(t) {
+  var result = babel.transform(sourceSrc, {
+    sourceMaps: 'inline',
+    filename: sourceFile,
+  });
+
+  var sm = convert
+    .fromSource(result.code.toString())
+    .toObject();
+
+  // With only "filename", the source path is the base name.
+  t.same(sm.sources, [path.basename(sourceFile)]);
+
+  t.done();
+});
+
+test('babel source maps (filename and sourceFileName)', function(t) {
+  var result = babel.transform(sourceSrc, {
+    sourceMaps: 'inline',
+    filename: sourceFile,
+    sourceFileName: sourceFile,
+  });
+
+  // With "sourceFileName", the source path is "sourceFileName".
+  var sm = convert
+    .fromSource(result.code.toString())
+    .toObject();
+
+  t.same(sm.sources, [sourceFile]);
+
+  t.done();
+});
