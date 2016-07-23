71a7cbf18a4d4e757ba86eec2342ff3916e3f4da
San <sang@wix.com>
2016-07-23 20:55:28 +0300

    fixed "sourceMap" to "sourceMaps"

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ Babelify.configure = function (opts) {
   opts = assign({}, opts);
   var extensions = opts.extensions ? babel.util.arrayify(opts.extensions) : null;
   var sourceMapRelative = opts.sourceMapRelative;
-  if (opts.sourceMap !== false) opts.sourceMap = "inline";
+  if (opts.sourceMaps !== false) opts.sourceMaps = "inline";
 
   // babelify specific options
   delete opts.sourceMapRelative;
