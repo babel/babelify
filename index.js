653305905179c4b08df4078969d740a10af7f648
Logan Smyth <loganfsmyth@gmail.com>
2018-09-02 21:17:04 -0700

    Add helpful logging to verify core version.

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
 var stream = require("stream");
-var babel  = require("@babel/core");
 var util   = require("util");
-var path   = require("path")
+var path   = require("path");
+
+let babel;
+try {
+  babel = require("@babel/core");
+} catch (err) {
+  if (err.code === "MODULE_NOT_FOUND") {
+    err.message +=
+      "\n babelify@9 requires Babel 7.x (the package '@babel/core'). " +
+      "If you'd like to use Babel 6.x ('babel-core'), you should install 'babel-loader@7'.";
+  }
+  throw err;
+}
+
+// Since we've got the reverse bridge package at @babel/core@6.x, give
+// people useful feedback if they try to use it alongside babel-loader.
+if (/^6\./.test(babel.version)) {
+  throw new Error(
+    "\n babel-loader@8 will not work with the '@babel/core@6' bridge package. " +
+      "If you want to use Babel 6.x, install 'babel-loader@7'.",
+  );
+}
 
 module.exports = Babelify;
 util.inherits(Babelify, stream.Transform);
